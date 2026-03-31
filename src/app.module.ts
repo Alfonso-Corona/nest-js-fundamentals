0594d842269dbe8e9c5efd4162d10f18ded5885f
Alfonso Corona <coronaalfonsor@gmail.com>
2026-03-30 19:27:41 -0600

    feat(*): create tag entity and db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './ormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(config), TagModule],
   controllers: [],
   providers: [AppService],
 })
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TagEntity } from './tag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TagService {
-  getAllTags() {
-    return ['ai', 'typescript', 'javascript'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  async getAllTags() {
+    return await this.tagRepository.find();
   }
 }
