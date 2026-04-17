2811c1004080e9262004699cfc4d70592a10f4cf
Alfonso Corona <coronaalfonsor@gmail.com>
2026-04-16 18:44:25 -0600

    feat(*): following methods implemented

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,12 +4,15 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProfileType } from './types/profile.type';
 import { IProfileResponse } from './types/profileResponse.interface';
+import { FollowEntity } from './follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   async getProfile(currentUserId: number, profileUsername: string) {
@@ -23,12 +26,47 @@ export class ProfileService {
       throw new HttpException('Profile not found', HttpStatus.NOT_FOUND);
     }
 
-    delete profile?.password;
-    delete profile?.email;
     return { ...profile, following: false };
   }
 
+  async followProfile(currentUserId: number, followingUsername: string) {
+    const followingProfile = await this.userRepository.findOne({
+      where: {
+        username: followingUsername,
+      },
+    });
+
+    if (!followingProfile) {
+      throw new HttpException('Profile not found', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === followingProfile.id) {
+      throw new HttpException(
+        "You can't follow yourself!!",
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: followingProfile.id,
+      },
+    });
+
+    if (!follow) {
+      const newFollow = new FollowEntity();
+      newFollow.followerId = currentUserId;
+      newFollow.followingId = followingProfile.id;
+      await this.followRepository.save(newFollow);
+    }
+
+    return { ...followingProfile, following: true };
+  }
+
   generateProfileResponse(profile: ProfileType): IProfileResponse {
+    delete profile?.password;
+    delete profile?.email;
     return { profile };
   }
 }
